90a4989a5202319b02ebdf2f611e6726463dbafb
Damine <Damine@Lenovo-PC>
2015-12-02 14:58:28 +0100

    Signed-off-by: Damine <Damine@Lenovo-PC>

--- gestionProjet/src/main/java/edu/esprit/gestion_projet/aop/Affichage.java
+++ gestionProjet/src/main/java/edu/esprit/gestion_projet/aop/Affichage.java
@@ -3,6 +3,8 @@ package edu.esprit.gestion_projet.aop;
 public class Affichage {
 	///test
 	/////test1
+	////test2
+	/////test3
 	public void debutExecution()
 	{
 		System.out.println("Debut execution de la requete");
